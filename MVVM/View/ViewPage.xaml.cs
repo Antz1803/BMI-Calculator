e20f26000cfdd485125689b9898d555e6c5f7824
Jeorge Rey M. Antipaso <134927159+Antz1803@users.noreply.github.com>
2024-11-06 09:15:18 +0800

    Update ViewPage.xaml.cs

--- MVVM/View/ViewPage.xaml.cs
+++ MVVM/View/ViewPage.xaml.cs
@@ -5,6 +5,7 @@ public partial class ViewPage : ContentPage
     public ViewPage()
     {
         InitializeComponent(); 
+        //Binding the model
         BindingContext = new BMI.MVVM.Model.ViewModel();
     }
 }
